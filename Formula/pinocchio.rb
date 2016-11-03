2b44cda8f778a51167f1401983add78df50caf23
jcarpent <jcarpent@laas.fr>
2016-11-03 11:39:19 +0100

    [pinocchio] Bump to version 1.2.1

--- Formula/pinocchio.rb
+++ Formula/pinocchio.rb
@@ -3,20 +3,26 @@ class Pinocchio < Formula
   homepage "https://stack-of-tasks.github.io/pinocchio"
 
   stable do
-    url "ftp://trac.laas.fr/pub/openrobots/pinocchio/pinocchio-1.2.0.tar.gz"
-    sha256 "4c2383e6fdc2cb2b3cdc2ef8409fe76674ccd1b83f8e9b5f8fd5b550d11085c4"
+    url "ftp://trac.laas.fr/pub/openrobots/pinocchio/pinocchio-1.2.1.tar.gz"
+    sha256 "36611a5db186bf7b95de6279dba5bbb69cd5716fa43b799221de578a166b6177"
+    
+    patch :p0 do
+      url "https://git.openrobots.org/projects/robotpkg-wip/repository/revisions/master/raw/pinocchio/patches/patch-aa"
+      sha256 "c9d3ffc9772e71582462ce5113c8c340cb1a7beeb47d1ce388a657440b8f40f9"
+    end
 
-    patch :p1, :DATA
+    patch :p0 do
+      url "https://git.openrobots.org/projects/robotpkg-wip/repository/revisions/master/raw/pinocchio/patches/patch-ab"
+      sha256 "252967f2aa09553595b54f8c2e5810c810fbc3083344681d1924d4d8c703f4f9"
+    end
   end
 
   head do
-    url "https://github.com/jcarpent/pinocchio.git", :branch => "devel"
+    url "https://github.com/jcarpent/pinocchio.git", :branch => "master"
   end
 
   devel do
-    url "ftp://trac.laas.fr/pub/openrobots/pinocchio/pinocchio-1.1.2.tar.gz"
-    sha256 "732f0643093e3c04190cf0ccab2e112332419b8f5de06fdcd600be57efe2f87a"
-    #url "https://github.com/stack-of-tasks/pinocchio/archive/devel.zip"
+    url "https://github.com/jcarpent/pinocchio.git", :branch => "devel"
   end
 
   option "without-python", "Build without Python support"
