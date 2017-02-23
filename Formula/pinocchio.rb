391715037be7571c3f3978fbdcc239d987f9aa5e
jcarpent <jcarpent@laas.fr>
2017-02-23 22:02:49 +0100

    [pinocchio] Bump to version 1.2.3

--- Formula/pinocchio.rb
+++ Formula/pinocchio.rb
@@ -3,18 +3,8 @@ class Pinocchio < Formula
   homepage "https://stack-of-tasks.github.io/pinocchio"
 
   stable do
-    url "ftp://trac.laas.fr/pub/openrobots/pinocchio/pinocchio-1.2.1.tar.gz"
-    sha256 "36611a5db186bf7b95de6279dba5bbb69cd5716fa43b799221de578a166b6177"
-    
-    patch :p0 do
-      url "https://git.openrobots.org/projects/robotpkg-wip/repository/revisions/master/raw/pinocchio/patches/patch-aa"
-      sha256 "c9d3ffc9772e71582462ce5113c8c340cb1a7beeb47d1ce388a657440b8f40f9"
-    end
-
-    patch :p0 do
-      url "https://git.openrobots.org/projects/robotpkg-wip/repository/revisions/master/raw/pinocchio/patches/patch-ab"
-      sha256 "252967f2aa09553595b54f8c2e5810c810fbc3083344681d1924d4d8c703f4f9"
-    end
+    url "ftp://trac.laas.fr/pub/openrobots/pinocchio/pinocchio-1.2.3.tar.gz"
+    sha256 "3d61ffd8fbd17baba66658fb9d7ccce48fe2b6af55546283d18af4ba31573fbd"
   end
 
   head do
