dc973c83d13704b37046dc982083196e5154a985
jcarpent <jcarpent@laas.fr>
2017-12-07 17:55:28 +0100

    gepetto-viewer-corba: new formula

---
+++ Formula/gepetto-viewer-corba.rb
@@ -0,0 +1,40 @@
+class GepettoViewerCorba < Formula
+  desc "An efficient library for Rigid Body Dynamics"
+  homepage "https://stack-of-tasks.github.io/pinocchio"
+  url "https://github.com/humanoid-path-planner/gepetto-viewer-corba"
+
+  stable do
+    url "https://github.com/humanoid-path-planner/gepetto-viewer-corba/releases/download/v2.0.0/gepetto-viewer-corba-2.0.0.tar.gz"
+    sha256 "39ef619e57747656b76666863052607818d1fe0fd789baabf1d631671c19f412"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "doxygen" => :build
+  depends_on "boost"
+  depends_on "omniorb"
+  depends_on "gepetto-viewer"
+  depends_on "qt@4"
+  depends_on "osgqt"
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_PREFIX_PATH=#{Formula["qt"].opt_prefix}"
+    args << "-DCMAKE_PREFIX_PATH=#{Formula["osgqt"].opt_prefix}/lib/pkgconfig"
+    if build.devel?
+      system "git submodule cmake update --init"
+    end
+    mkdir "build" do
+      system "cmake", "..", *args, "-DBUILD_UNIT_TESTS=OFF"
+      system "make"
+      #system "make", "check"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
+
+__END__
