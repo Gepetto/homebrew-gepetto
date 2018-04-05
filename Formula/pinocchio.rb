89aeef9380bdaf7d631d454f7ea7b4d1643db044
jcarpent <jcarpent@laas.fr>
2018-04-05 15:15:50 +0200

    pinocchio: update 1.2.7

--- Formula/pinocchio.rb
+++ Formula/pinocchio.rb
@@ -3,8 +3,10 @@ class Pinocchio < Formula
   homepage "https://stack-of-tasks.github.io/pinocchio"
 
   stable do
-    url "http://www.openrobots.org/distfiles/pinocchio/pinocchio-1.2.6.tar.gz"
-    sha256 "6580ff23743a72633678b9c4109891aa1830bd271cde7858652f7ca127fb9be8"
+    url "http://www.openrobots.org/distfiles/pinocchio/pinocchio-1.2.7.tar.gz"
+    sha256 "0d3586a9aa144121c893b27ea8c1129c51e5f2b11f054379ae60e5637a2089a1"
+    
+    patch :DATA
   end
 
   #head do
@@ -27,7 +29,7 @@ class Pinocchio < Formula
   depends_on "boost-python" => :recommended if build.with? "python"
   depends_on "urdfdom" => :recommended
   depends_on "eigenpy" => :recommended if build.with? "python"
-  depends_on "python" => :recommended if build.with? "python"
+  depends_on "python@2" => :recommended if build.with? "python"
   depends_on "numpy" => :recommended if build.with? "python"
   depends_on "hpp-fcl" => :recommended if build.with? "fcl"
 
