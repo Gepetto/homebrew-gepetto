6df2b4cf9c12835d92f914d00ba013260abbc33a
Justin Carpentier <jcarpent@laas.fr>
2018-07-19 10:24:30 +0200

    eigenpy: update 1.4.4

--- Formula/eigenpy.rb
+++ Formula/eigenpy.rb
@@ -1,8 +1,8 @@
 class Eigenpy < Formula
   desc "Python bindings of Eigen library with Numpy support."
   homepage "https://github.com/stack-of-tasks/eigenpy"
-  url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.4.3/eigenpy-1.4.3.tar.gz"
-  sha256 "f715e8e2c208a922d67a25da0b2afb90b05f6b7987d8de6c6d37b5f1658d8a58"
+  url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.4.4/eigenpy-1.4.4.tar.gz"
+  sha256 "dd9280d1263e2e110bbb1ddc0eb9dd515b3c5d15fadc104b3d1b32904fe2e4e7"
   head "https://github.com/stack-of-tasks/eigenpy.git", :branch => "devel"
 
   depends_on :xcode => :build
