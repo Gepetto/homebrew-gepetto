399184095c6bfd59ef6bce365ad31795ce6855c7
Justin Carpentier <jcarpent@laas.fr>
2018-08-27 23:12:17 +0200

    eigenpy: add bottle

--- Formula/eigenpy.rb
+++ Formula/eigenpy.rb
@@ -5,6 +5,13 @@ class Eigenpy < Formula
   sha256 "dd9280d1263e2e110bbb1ddc0eb9dd515b3c5d15fadc104b3d1b32904fe2e4e7"
   head "https://github.com/stack-of-tasks/eigenpy.git", :branch => "devel"
 
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.4.4"
+
+    sha256 "5444bb2b3c778d2d60110fd00092eae53d3ecb0155db6b4e0adeaa9b3afb68af" => :high_sierra
+  end 
+
   depends_on :xcode => :build
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
@@ -13,8 +20,6 @@ class Eigenpy < Formula
   depends_on "boost-python"
   depends_on "numpy"
   depends_on "python@2"
-
-  patch :DATA
 
   def install
     mkdir "build" do
@@ -31,4 +36,3 @@ class Eigenpy < Formula
     system "false"
   end
 end
-__END__
