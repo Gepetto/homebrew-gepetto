1071452a2697d1984c71b75215e726f87a326532
Justin Carpentier <justin.carpentier@inria.fr>
2019-03-04 13:42:35 +0100

    hpp-fcl: update bottle

--- Formula/hpp-fcl.rb
+++ Formula/hpp-fcl.rb
@@ -8,7 +8,8 @@ class HppFcl < Formula
 
   bottle do
     root_url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.0.1"
-    sha256 "cb7b42f722427185ce9e215bcc020c3d51d20f5f715cb6141135bb54d8a34f24" => :mojave
+    rebuild 1
+    sha256 "1735294a013ffb4bd6e57407b4cd96c4158c5cedac6e3e0d3545fdae256b39d1" => :mojave
   end 
 
   depends_on "cmake" => :build
@@ -17,6 +18,7 @@ class HppFcl < Formula
   depends_on "assimp"
   depends_on "eigen"
   depends_on "octomap"
+  depends_on "boost"
   depends_on "brewsci/homebrew-science/cddlib"
 
   def install
