08df5baa6de2f0a38835d7b51f1268b10146db00
Justin Carpentier <justin.carpentier@inria.fr>
2019-03-06 11:31:22 +0100

    gepetto-viewer-corba: update 5.1.3

--- Formula/gepetto-viewer-corba.rb
+++ Formula/gepetto-viewer-corba.rb
@@ -1,41 +1,44 @@
 class GepettoViewerCorba < Formula
   desc "Graphical Interface for Pinocchio and HPP"
-  homepage "https://github.com/humanoid-path-planner/gepetto-viewer"
+  homepage "https://github.com/humanoid-path-planner/gepetto-viewer-corba"
   url "https://github.com/humanoid-path-planner/gepetto-viewer-corba"
 
   stable do
-    url "https://github.com/humanoid-path-planner/gepetto-viewer-corba/releases/download/v2.3.1/gepetto-viewer-corba-2.3.1.tar.gz"
-    sha256 "214f5038aeba74a64b548f55c429401d08a43672682146b111c3ddfec8c7c4eb"
+    url "https://github.com/humanoid-path-planner/gepetto-viewer-corba/releases/download/v5.1.3/gepetto-viewer-corba-5.1.3.tar.gz"
+    sha256 "ff774b69b0d28021db5f1a6285bbab2f0ef2cd09babc0847e3e7a4b977370e9b"
   end
 
   bottle do
-    cellar :any_skip_relocation
-    root_url "https://github.com/humanoid-path-planner/gepetto-viewer-corba/releases/download/v2.3.1"
-    
-    rebuild 1
-    sha256 "e4cf462bb1a9f32506dafc4f472f2aae04211a875c5485d0abeb8bc5447ff8dc" => :high_sierra
-  end 
+    root_url "https://github.com/humanoid-path-planner/gepetto-viewer-corba/releases/download/v5.1.3"
+    sha256 "f820c2ec61e940c8e6d1b9d9d645fd74d14a8438d58da6e2b14f0928b39ab205" => :mojave
+  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "doxygen" => :build
   depends_on "boost"
-  depends_on "cartr/qt4/qt@4"
+  depends_on "qt"
   depends_on "omniorb"
   depends_on "gepetto-viewer"
   depends_on "osgqt"
+  depends_on "python@2" => :recommended if build.with? "python"
 
   def install
-    args = std_cmake_args
-    args << "-DCMAKE_PREFIX_PATH=#{Formula["qt@4"].opt_bin}"
-    args << "-DCMAKE_PREFIX_PATH=#{Formula["osgqt"].opt_prefix}/lib/pkgconfig"
     if build.devel?
       system "git submodule cmake update --init"
     end
+    
+    pyver = Language::Python.major_minor_version "python2"
+    py_prefix = Formula["python2"].opt_frameworks/"Python.framework/Versions/#{pyver}"
+
     mkdir "build" do
-      system "cmake", "..", *args, "-DBUILD_UNIT_TESTS=OFF"
+      args = *std_cmake_args
+      args << "-DCMAKE_PREFIX_PATH=#{Formula["qt"].opt_bin}"
+      args << "-DCMAKE_PREFIX_PATH=#{Formula["osgqt"].opt_prefix}/lib/pkgconfig"
+      args << "-DPYTHON_LIBRARY=#{py_prefix}/lib"
+      args << "-DBUILD_UNIT_TESTS=OFF"
+      system "cmake", "..", *args
       system "make"
-      #system "make", "check"
       system "make", "install"
     end
   end
