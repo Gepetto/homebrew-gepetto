18f4be6abe88af069c9719bd576e06c99d4624b6
Justin Carpentier <justin.carpentier@inria.fr>
2019-06-28 12:31:07 +0200

    eigenpy: update 1.5.2

--- Formula/eigenpy.rb
+++ Formula/eigenpy.rb
@@ -1,14 +1,14 @@
 class Eigenpy < Formula
   desc "Python bindings of Eigen library with Numpy support."
   homepage "https://github.com/stack-of-tasks/eigenpy"
-  url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.5.0/eigenpy-1.5.0.tar.gz"
-  sha256 "4385bf1b8f8624a584022817b41e6b3e300a5e942f974e3c9d41643fafd0bf74"
+  url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.5.2/eigenpy-1.5.2.tar.gz"
+  sha256 "4fc8af60ee9bce843b66d68eb8beee184611c692528a4f99be75b7fcaa109d39"
   head "https://github.com/stack-of-tasks/eigenpy.git", :branch => "devel"
 
   bottle do
-    root_url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.5.0"
-    sha256 "f56543ea3f9e74970b69ba801f9f38b54e77c73e6dc3d84804583b542a9de681" => :mojave
-  end 
+    root_url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.5.2/eigenpy-1.5.2.mojave.bottle.tar.gz"
+    sha256 "1797167b9ea4bb456d894a36a1b5251badd1cef5b33fbb766ae50ec26c3a1e44" => :mojave
+  end
 
   depends_on :xcode => :build
   depends_on "cmake" => :build
