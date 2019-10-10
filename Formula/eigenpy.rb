4df0edd4690b88924f7b3da6d58d685df0246ae1
Justin Carpentier <justin.carpentier@inria.fr>
2019-10-10 08:30:18 +0200

    eigenpy: update bottle

--- Formula/eigenpy.rb
+++ Formula/eigenpy.rb
@@ -6,8 +6,9 @@ class Eigenpy < Formula
   head "https://github.com/stack-of-tasks/eigenpy.git", :branch => "devel"
 
   bottle do
-    sha256 "9fffd4df1e737dbe47db2c737d7725a0287ccc02ee5f9a81424f2d356b41520b" => :mojave
-    root_url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.6.0/eigenpy-1.6.0.mojave.bottle.tar.gz"
+    rebuild 1
+    root_url "https://github.com/stack-of-tasks/eigenpy/releases/download/v1.6.0/eigenpy-1.6.0.mojave.bottle.1.tar.gz"
+    sha256 "8e541a006c1f3099573a51854f82d39b2942b2747c1bb228b679870d5c478d87" => :mojave
   end
 
   depends_on :xcode => :build
@@ -16,12 +17,17 @@ class Eigenpy < Formula
   depends_on "doxygen" => :build
   depends_on "eigen"
   depends_on "boost-python"
-  depends_on "numpy"
+  depends_on "numpy@1.16"
   depends_on "python@2"
 
   def install
+    pyver = Language::Python.major_minor_version "python2"
+    py_prefix = Formula["python2"].opt_frameworks/"Python.framework/Versions/#{pyver}"
     mkdir "build" do
       args = *std_cmake_args
+      args << "-DPYTHON_INCLUDE_DIR=#{py_prefix}/include/python#{pyver}"
+      args << "-DPYTHON_LIBRARY=#{py_prefix}/lib"
+      args << "-DPYTHON_EXECUTABLE=#{py_prefix}/bin/python2"
       system "cmake", "..", *args
       system "make"
       system "make", "install"
