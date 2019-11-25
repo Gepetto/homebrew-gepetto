f91e7c68c92c4859d280712417ee244e758031d7
Justin Carpentier <justin.carpentier@inria.fr>
2019-11-25 11:54:30 +0100

    hpp-fcl: update 1.2.0

--- Formula/hpp-fcl.rb
+++ Formula/hpp-fcl.rb
@@ -1,16 +1,16 @@
 class HppFcl < Formula
   desc "An extension of the Flexible Collision Library"
   homepage "https://github.com/humanoid-path-planner/hpp-fcl"
-  url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.1.2/hpp-fcl-1.1.2.tar.gz"
-  sha256 "f1636895054cd2cf08efbde35b88505f6d973380d9605f408fb65f26f5228f89"
-  patch :DATA
+  url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.2.0/hpp-fcl-1.2.0.tar.gz"
+  sha256 "1df98a3bf49df379203f5d43b00edd2ce5f23a7ae6c61ee3ecfdf6da0adb9fdc"
 
   head "https://github.com/humanoid-path-planner/hpp-fcl", :branch => "master"
 
+  option "without-python", "Build without Python support"
+
   bottle do
-    rebuild 1
-    root_url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.1.2"
-    sha256 "bcb6b2077fe8252347f6d404358dc34dae265fe8fa42c0cf4d724eeb69bd2315" => :mojave
+    root_url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.2.0"
+    sha256 "a600b82273213c48cb446ba84317edb8a22b6ed27896d4a5dee35f95c04b4e14" => :mojave
   end 
 
   depends_on "cmake" => :build
@@ -21,10 +21,28 @@ class HppFcl < Formula
   depends_on "octomap"
   depends_on "boost"
   depends_on "brewsci/homebrew-science/cddlib"
+  depends_on "boost-python" => :recommended if build.with? "python"
+  depends_on "eigenpy" => :recommended if build.with? "python"
+  depends_on "python@2" => :recommended if build.with? "python"
 
   def install
+    if build.head?
+      system "git submodule update --init"
+      system "git pull --unshallow --tags" 
+    end 
+
+    pyver = Language::Python.major_minor_version "python2"
+    py_prefix = Formula["python2"].opt_frameworks/"Python.framework/Versions/#{pyver}"
+
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      args = *std_cmake_args
+      args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+      args << "-DBUILD_PYTHON_INTERFACE=ON"
+      args << "-DPYTHON_INCLUDE_DIR=#{py_prefix}/include/python#{pyver}"
+      args << "-DPYTHON_LIBRARY=#{py_prefix}/lib"
+      args << "-DPYTHON_EXECUTABLE=#{py_prefix}/bin/python2"
+
+      system "cmake", "..", *args
       system "make"
       system "make", "install"
     end
@@ -34,33 +52,3 @@ class HppFcl < Formula
     system "false"
   end
 end
-__END__
-diff --git a/include/hpp/fcl/mesh_loader/assimp.h b/include/hpp/fcl/mesh_loader/assimp.h
-index c769d67..fe751ce 100644
---- a/include/hpp/fcl/mesh_loader/assimp.h
-+++ b/include/hpp/fcl/mesh_loader/assimp.h
-@@ -3,7 +3,8 @@
-  *
-  *  Copyright (c) 2011-2014, Willow Garage, Inc.
-  *  Copyright (c) 2014-2015, Open Source Robotics Foundation
-- *  Copyright (c) 2016, CNRS - LAAS
-+ *  Copyright (c) 2016-2019, CNRS - LAAS
-+ *  Copyright (c) 2019, INRIA
-  *  All rights reserved.
-  *
-  *  Redistribution and use in source and binary forms, with or without
-@@ -37,6 +38,14 @@
- #ifndef HPP_FCL_MESH_LOADER_ASSIMP_H
- #define HPP_FCL_MESH_LOADER_ASSIMP_H
- 
-+// Assimp >= 5.0 is forcing the use of C++11 keywords. A fix has been submitted https://github.com/assimp/assimp/pull/2758.
-+// The next lines fixes the bug for current version of hpp-fcl.
-+#include <assimp/defs.h>
-+#if __cplusplus < 201103L && defined(AI_NO_EXCEPT)
-+  #undef AI_NO_EXCEPT
-+  #define AI_NO_EXCEPT
-+#endif
-+
- #ifdef HPP_FCL_USE_ASSIMP_UNIFIED_HEADER_NAMES
-   #include <assimp/DefaultLogger.hpp>
-   #include <assimp/IOStream.hpp>
