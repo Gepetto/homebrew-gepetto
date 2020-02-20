30ef2bb02b293b5774f4ac9a241821158b9af244
Justin Carpentier <justin.carpentier@inria.fr>
2020-02-20 18:19:34 +0100

    pinocchio@2: update 2.3.1

--- Formula/pinocchio@2.rb
+++ Formula/pinocchio@2.rb
@@ -2,13 +2,12 @@ class PinocchioAT2 < Formula
   desc "An efficient and fast C++ library implementing Rigid Body Dynamics algorithms"
   homepage "https://stack-of-tasks.github.io/pinocchio"
   head "https://github.com/stack-of-tasks/pinocchio.git", :branch => "devel"
-
-  url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.3.0/pinocchio-2.3.0.tar.gz"
-  sha256 "9bab5178096497e900a76bc4e88ad65bfb143256b719a35f3fc16a00a6a4dc49"
+  url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.3.1/pinocchio-2.3.1.tar.gz"
+  sha256 "7fe811176916927aa60e05efa8db3715187ea53263b73dc57391d9d237779382"
 
   bottle do
-    root_url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.3.0"
-    sha256 "2b50ddc989f67ac450d8770c7549e9632b3f4327f07d46ad1177923344852278" => :mojave
+    root_url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.3.1"
+    sha256 "3293fc0ddd6b0605a91c4a9e1885ae952c603ef301608b731efae0cb647fd33c" => :mojave
   end
 
   option "without-python", "Build without Python support"
@@ -21,9 +20,9 @@ class PinocchioAT2 < Formula
   depends_on "eigen"
   depends_on "urdfdom" => :recommended
   depends_on "boost-python" => :recommended if build.with? "python"
-  depends_on "eigenpy" => :recommended if build.with? "python"
+  depends_on "eigenpy@2" => :recommended if build.with? "python"
   depends_on "python@2" => :recommended if build.with? "python"
-  depends_on "hpp-fcl" => :recommended if build.with? "fcl"
+  depends_on "hpp-fcl@2" => :recommended if build.with? "fcl"
 
   def install
     if build.head?
@@ -36,11 +35,9 @@ class PinocchioAT2 < Formula
     
     mkdir "build" do
       args = *std_cmake_args
-      args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
-      args << "-DCMAKE_BUILD_TYPE=Release"
       args << "-DPYTHON_EXECUTABLE=#{py_prefix}/bin/python2"
-      args << "-DBUILD_UNIT_TESTS=OFF"
       args << "-DBUILD_WITH_COLLISION_SUPPORT=ON"
+      args << "-DBUILD_UNIT_TESTS=OFF"
       system "cmake", "..", *args
       system "make"
       system "make", "install"
