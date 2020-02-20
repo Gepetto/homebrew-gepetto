516e15c435f1f2cf93aecb6fc7735b4dcefeef21
Justin Carpentier <justin.carpentier@inria.fr>
2020-02-20 17:35:19 +0100

    hpp-fcl@2: update 1.3.0

--- Formula/hpp-fcl@2.rb
+++ Formula/hpp-fcl@2.rb
@@ -1,16 +1,16 @@
-class HppFcl < Formula
+class HppFclAT2 < Formula
   desc "An extension of the Flexible Collision Library"
   homepage "https://github.com/humanoid-path-planner/hpp-fcl"
-  url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.2.2/hpp-fcl-1.2.2.tar.gz"
-  sha256 "2b60468f137e0045c60d8561836f6c714838cf891369ec96d5ed945ad824b6f2"
+  url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.3.0/hpp-fcl-1.3.0.tar.gz"
+  sha256 "ffdd5487ef93ba0ac190807d1ef71528e380803a46a69204916944f5df82b218"
 
   head "https://github.com/humanoid-path-planner/hpp-fcl", :branch => "master"
 
   option "without-python", "Build without Python support"
 
   bottle do
-    root_url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.2.2"
-    sha256 "be55c49b507894f882b4e8baafd59f20ef53e58c19fac8cca1355d3804c7c08a" => :mojave
+    root_url "https://github.com/humanoid-path-planner/hpp-fcl/releases/download/v1.3.0"
+    sha256 "d8074e73a962b108c23e7696f81e2c7d88bf7d7bbe5ddbd82c61003c85df61fd" => :mojave
   end 
 
   depends_on "cmake" => :build
@@ -22,7 +22,7 @@ class HppFcl < Formula
   depends_on "boost"
   depends_on "brewsci/homebrew-science/cddlib"
   depends_on "boost-python" => :recommended if build.with? "python"
-  depends_on "eigenpy" => :recommended if build.with? "python"
+  depends_on "eigenpy@2" => :recommended if build.with? "python"
   depends_on "python@2" => :recommended if build.with? "python"
 
   def install
@@ -32,14 +32,10 @@ class HppFcl < Formula
     end 
 
     pyver = Language::Python.major_minor_version "python2"
-    py_prefix = Formula["python2"].opt_frameworks/"Python.framework/Versions/#{pyver}"
+    py_prefix = Formula["python@2"].opt_frameworks/"Python.framework/Versions/#{pyver}"
 
     mkdir "build" do
       args = *std_cmake_args
-      args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
-      args << "-DBUILD_PYTHON_INTERFACE=ON"
-      args << "-DPYTHON_INCLUDE_DIR=#{py_prefix}/include/python#{pyver}"
-      args << "-DPYTHON_LIBRARY=#{py_prefix}/lib"
       args << "-DPYTHON_EXECUTABLE=#{py_prefix}/bin/python2"
 
       system "cmake", "..", *args
