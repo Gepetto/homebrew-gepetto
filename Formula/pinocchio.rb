c80029947a2f6bf340b53dc56ba1a35b8757bd84
Justin Carpentier <justin.carpentier@inria.fr>
2020-02-20 18:19:51 +0100

    pinocchio: update 2.3.1

--- Formula/pinocchio.rb
+++ Formula/pinocchio.rb
@@ -2,13 +2,12 @@ class Pinocchio < Formula
   desc "An efficient and fast C++ library implementing Rigid Body Dynamics algorithms"
   homepage "https://stack-of-tasks.github.io/pinocchio"
   head "https://github.com/stack-of-tasks/pinocchio.git", :branch => "devel"
-
-  url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.3.0/pinocchio-2.3.0.tar.gz"
-  sha256 "9bab5178096497e900a76bc4e88ad65bfb143256b719a35f3fc16a00a6a4dc49"
+  url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.3.1/pinocchio-2.3.1.tar.gz"
+  sha256 "7fe811176916927aa60e05efa8db3715187ea53263b73dc57391d9d237779382"
 
   bottle do
-    root_url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.3.0"
-    sha256 "3421d4b6df672646fb9db92991abac3e2a0b3b9b61799cadceef999841acd8cf" => :mojave
+    root_url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.3.1"
+    sha256 "719271801dc15ced7fc907a0ce0730ba137f5539f0af4fd58c8a527698360875" => :mojave
   end
 
   option "without-python", "Build without Python support"
@@ -21,10 +20,10 @@ class Pinocchio < Formula
   depends_on "eigen"
   depends_on "boost-python3" => :recommended if build.with? "python"
   depends_on "urdfdom" => :recommended
-  depends_on "eigenpy-python3" => :recommended if build.with? "python"
+  depends_on "eigenpy" => :recommended if build.with? "python"
   depends_on "python" => :recommended if build.with? "python"
   depends_on "numpy" => :recommended if build.with? "python"
-  depends_on "hpp-fcl-python3" => :recommended if build.with? "fcl"
+  depends_on "hpp-fcl" => :recommended if build.with? "fcl"
 
   def install
     if build.head?
@@ -37,8 +36,6 @@ class Pinocchio < Formula
     
     mkdir "build" do
       args = *std_cmake_args
-      args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
-      args << "-DCMAKE_BUILD_TYPE=Release"
       args << "-DPYTHON_EXECUTABLE=#{py_prefix}/bin/python#{pyver}"
       args << "-DBUILD_WITH_COLLISION_SUPPORT=ON"
       args << "-DBUILD_UNIT_TESTS=OFF"
