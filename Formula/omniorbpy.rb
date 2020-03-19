5eaa50b29dafc48fc4edc74072c196b60e45cc62
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-19 17:31:06 +0100

    omniorbpy: patch python3

--- Formula/omniorbpy.rb
+++ Formula/omniorbpy.rb
@@ -4,10 +4,13 @@ class Omniorbpy < Formula
   url "https://downloads.sourceforge.net/project/omniorb/omniORBpy/omniORBpy-4.2.3/omniORBpy-4.2.3.tar.bz2"
   sha256 "5c601888e57c7664324357a1be50f2739c468057b46fba29821a25069fc0aee5"
 
+  patch :DATA
+
   bottle do
     cellar :any
     root_url "https://github.com/Gepetto/omniORBpy/releases/download/v4.2.3"
-    sha256 "e3d01c03cad7edb7ec11dd199b91b23f9d9c38fcd19e35b294741c509ebf4200" => :mojave
+    rebuild 1
+    sha256 "55612da3b1f7d35da3a5a9b634541045152f80e5c52af7260fc8349e3743744a" => :mojave
   end
 
   depends_on "pkg-config" => :build
@@ -29,3 +32,35 @@ class Omniorbpy < Formula
     system false
   end
 end
+__END__
+diff a/python3/omniidl_be/python.py b/python3/omniidl_be/python.py
+index ade8d11..e5515b6 100644
+--- a/python3/omniidl_be/python.py
++++ b/python3/omniidl_be/python.py
+@@ -2060,21 +2060,21 @@ class DocstringVisitor (idlvisitor.AstVisitor):
+ 
+     def visitOperation(self, node):
+         if node.identifier() == self.target_id:
+-            sn = node.scopedName() + ["im_func"]
+-            sn[-3] = "_objref_" + sn[-3]
++            sn = node.scopedName()
++            sn[-2] = "_objref_" + sn[-2]
+             self.docs.append((sn, self.target_node.scopedName()))
+             self.ok = 1
+ 
+     def visitAttribute(self, node):
+         for n in node.declarators():
+             if n.identifier() == self.target_id:
+-                sn = n.scopedName() + ["im_func"]
+-                sn[-3] = "_objref_" + sn[-3]
+-                sn[-2] = "_get_"    + sn[-2]
++                sn = n.scopedName()
++                sn[-2] = "_objref_" + sn[-2]
++                sn[-1] = "_get_"    + sn[-1]
+                 self.docs.append((sn, self.target_node.scopedName()))
+                 if not node.readonly():
+                     sn = sn[:]
+-                    sn[-2] = "_set_" + n.identifier()
++                    sn[-1] = "_set_" + n.identifier()
+                     self.docs.append((sn, self.target_node.scopedName()))
+                 self.ok = 1
