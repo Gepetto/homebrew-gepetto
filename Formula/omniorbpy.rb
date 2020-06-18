17a10532408f85c014172b34e699af48358e83ca
Justin Carpentier <justin.carpentier@inria.fr>
2020-06-18 18:54:51 +0200

    omniorbby: update 4.2.4

--- Formula/omniorbpy.rb
+++ Formula/omniorbpy.rb
@@ -1,25 +1,22 @@
 class Omniorbpy < Formula
   desc "IOR and naming service utilities for omniORB"
   homepage "https://omniorb.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/omniorb/omniORBpy/omniORBpy-4.2.3/omniORBpy-4.2.3.tar.bz2"
-  sha256 "5c601888e57c7664324357a1be50f2739c468057b46fba29821a25069fc0aee5"
-
-  patch :DATA
+  url "https://downloads.sourceforge.net/project/omniorb/omniORBpy/omniORBpy-4.2.4/omniORBpy-4.2.4.tar.bz2"
+  sha256 "dae8d867559cc934002b756bc01ad7fabbc63f19c2d52f755369989a7a1d27b6"
 
   bottle do
     cellar :any
-    root_url "https://github.com/Gepetto/omniORBpy/releases/download/v4.2.3"
-    rebuild 1
-    sha256 "55612da3b1f7d35da3a5a9b634541045152f80e5c52af7260fc8349e3743744a" => :mojave
+    root_url "https://github.com/Gepetto/omniORBpy/releases/download/v4.2.4"
+    sha256 "35129cd8a0920dd0b82956b07c73fc12bbc20dabd72b8ce8623d7ce4df9e4e33" => :mojave
   end
 
   depends_on "pkg-config" => :build
   depends_on "omniorb"
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
-    pyver = Language::Python.major_minor_version "python3"
-    py_prefix = Formula["python3"].opt_frameworks/"Python.framework/Versions/#{pyver}"
+    pyver = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
+    py_prefix = Formula["python@3.8"].opt_frameworks/"Python.framework/Versions/#{pyver}"
     omniorb = Formula["omniorb"].opt_prefix
     
     ENV["PYTHON"] = "#{py_prefix}/bin/python#{pyver}"
@@ -32,35 +29,3 @@ class Omniorbpy < Formula
     system false
   end
 end
-__END__
-diff a/python3/omniidl_be/python.py b/python3/omniidl_be/python.py
-index ade8d11..e5515b6 100644
---- a/python3/omniidl_be/python.py
-+++ b/python3/omniidl_be/python.py
-@@ -2060,21 +2060,21 @@ class DocstringVisitor (idlvisitor.AstVisitor):
- 
-     def visitOperation(self, node):
-         if node.identifier() == self.target_id:
--            sn = node.scopedName() + ["im_func"]
--            sn[-3] = "_objref_" + sn[-3]
-+            sn = node.scopedName()
-+            sn[-2] = "_objref_" + sn[-2]
-             self.docs.append((sn, self.target_node.scopedName()))
-             self.ok = 1
- 
-     def visitAttribute(self, node):
-         for n in node.declarators():
-             if n.identifier() == self.target_id:
--                sn = n.scopedName() + ["im_func"]
--                sn[-3] = "_objref_" + sn[-3]
--                sn[-2] = "_get_"    + sn[-2]
-+                sn = n.scopedName()
-+                sn[-2] = "_objref_" + sn[-2]
-+                sn[-1] = "_get_"    + sn[-1]
-                 self.docs.append((sn, self.target_node.scopedName()))
-                 if not node.readonly():
-                     sn = sn[:]
--                    sn[-2] = "_set_" + n.identifier()
-+                    sn[-1] = "_set_" + n.identifier()
-                     self.docs.append((sn, self.target_node.scopedName()))
-                 self.ok = 1
