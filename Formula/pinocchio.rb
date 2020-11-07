f6ca00e26d2ba33a1bf91b01e4d3d778a44c7bdb
Justin Carpentier <justin.carpentier@inria.fr>
2020-11-07 19:21:20 +0100

    pinocchio: update 2.5.2

--- Formula/pinocchio.rb
+++ Formula/pinocchio.rb
@@ -2,12 +2,12 @@ class Pinocchio < Formula
   desc "An efficient and fast C++ library implementing Rigid Body Dynamics algorithms"
   homepage "https://stack-of-tasks.github.io/pinocchio"
   head "https://github.com/stack-of-tasks/pinocchio.git", :branch => "devel"
-  url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.4.7/pinocchio-2.4.7.tar.gz"
-  sha256 "fd33adabc4dc1a2d81dcdcaab535d8fef8d5a7ddc7962765473215a32e396e70"
+  url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.5.2/pinocchio-2.5.2.tar.gz"
+  sha256 "0eeb8e7d749e1b7c22fe6eb8515ec66670f5875c15138fc574a30b6a1b3f6170"
 
   bottle do
-    root_url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.4.7"
-    sha256 "fe164a4558c80a51fdbd1c57eb36d0663a05ccfcf0c6b9b68e6a1f2bdc374510" => :mojave
+    root_url "https://github.com/stack-of-tasks/pinocchio/releases/download/v2.5.2"
+    sha256 "d0ff16884c95e03961eb779af43002f5191d6c866959a1af04ab492af0acfd16" => :mojave
   end
 
   option "without-python", "Build without Python support"
@@ -21,7 +21,7 @@ class Pinocchio < Formula
   depends_on "boost-python3" => :recommended if build.with? "python"
   depends_on "urdfdom" => :recommended
   depends_on "eigenpy" => :recommended if build.with? "python"
-  depends_on "python@3.8" => :recommended if build.with? "python"
+  depends_on "python@3.9" => :recommended if build.with? "python"
   depends_on "numpy" => :recommended if build.with? "python"
   depends_on "hpp-fcl" => :recommended if build.with? "hpp-fcl"
 
@@ -31,8 +31,8 @@ class Pinocchio < Formula
       system "git pull --unshallow --tags" 
     end
 
-    pyver = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
-    py_prefix = Formula["python@3.8"].opt_frameworks/"Python.framework/Versions/#{pyver}"
+    pyver = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    py_prefix = Formula["python@3.9"].opt_frameworks/"Python.framework/Versions/#{pyver}"
     
     mkdir "build" do
       args = *std_cmake_args
